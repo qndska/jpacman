a076c5249df4e48e51e1e8f28450af910ad1c083
Sasasuang Pattanakitjaroenchai <106175374+qndska@users.noreply.github.com>
2024-09-16 17:23:38 +0700

    Test1x1BoardWithNullSquare

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,9 +1,12 @@
 package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+
     @Test
     void test1x1Board() {
         // Create a 1x1 grid with a BasicSquare
@@ -20,4 +23,18 @@ public class BoardTest {
         // Assert that the square at (0, 0) is the BasicSquare we created
         assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
     }
+
+    @Test
+    void test1x1BoardWithNullSquare() {
+        // Create a 1x1 grid with a null Square
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+
+        // Assert that creating the board throws an AssertionError
+        assertThrows(AssertionError.class, () -> {
+            new Board(grid); // This should throw an AssertionError
+        });
+    }
 }
+
+
